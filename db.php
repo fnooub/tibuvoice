3f10f1b2a209c35eec8cd0aaca3e3e366038ca07
tusachdotorg <54848935+tusachdotorg@users.noreply.github.com>
2019-11-23 17:11:45 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 
-$db = new PDO("mysql:host=w29ifufy55ljjmzq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;charset=utf8;dbname=fholksevs4yqxpbg", "xeiyjyiutd1geuch", "izvlfumonqatx3oq");
+$db = new PDO("mysql:host=ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;charset=utf8;dbname=yh4lr3h8gm2cajag", "lwihvoq7afr4bdps", "piyz5km4uie0x9pc");
 
 ?>
